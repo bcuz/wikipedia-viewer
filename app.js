e9419746e9c6ea69b6896fff743e3afcb999cde9
Adam Nawrocki <adam.nawroc@gmail.com>
2016-10-02 08:39:30 -0400

    an attempt

--- app.js
+++ app.js
@@ -1,4 +1,31 @@
 $(function() {
 
+    // var getData = function()
 
+  //   var queryData = {
+  //   action: "query",
+  //   titles: "warner",
+  //   format: "json"
+  // };
+
+    $.ajax({
+    type: 'GET',
+    url: "https://en.wikipedia.org/w/api.php",
+    data: {
+    action: "query",
+    // titles: "warner",
+    format: "json"
+  },
+    dataType: 'json',
+    // headers: { 'Api-User-Agent': 'Example/1.0' }
+    success: function( jsondata ){
+    console.log(( jsondata.result ));
+  }
+})
+//     .done(function(result){
+//     console.log(result);
+// })
+//   .fail(function(jqXHR, error){ //this waits for the ajax to return with an error promise object
+//     // console.log(jqXHR);
+//   });
 });
